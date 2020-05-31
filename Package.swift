a44e3d9569a5d0aebee6a853c800e6b8e28f1401
Henrik Panhans <henrik@panhans.dev>
2020-05-31 18:16:38 +0200

    Removed exposed library

--- Package.swift
+++ Package.swift
@@ -9,7 +9,6 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .executable(name: "imgurr", targets: ["Imgurr"]),
-        .library(name: "ImgurrCore", targets: ["ImgurrCore"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
